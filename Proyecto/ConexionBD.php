53ff7d36ea6ad9d8035bdb9ec097a70f4c086f03
SilverBaker <silverbaker@outlook.es>
2022-11-11 15:40:12 +0100

    Actualizado BD

--- Proyecto/ConexionBD.php
+++ Proyecto/ConexionBD.php
@@ -1,40 +1,21 @@
 <?php
+
 $bd;
 $permitirInstancianueva;
-$cadenaConexion = 'mysql:proyecto=proyecto;host=127.0.0.1';
+$cadenaConexion = 'mysql:dbname=proyecto;host=127.0.0.1';
 $usuario = filter_input(INPUT_POST, "usuario");
 $pass = filter_input(INPUT_POST, "clave");
+$c = 0;
 
-function conectarBD($cadenaConexion, $usuario, $pass) {
-    try {
-        $bd = new PDO($cadenaConexion, $usuario, $pass);
-        return $bd;
-    } catch (Exception $ex) {
-        echo "Contraseña incorrecta";
-    }
-}
-
-$bd=conectarBD($cadenaConexion, $usuario, $pass);
-
-
-function consulta($consulta){
-    $ins="$consulta";
+function consulta($consulta, $cadenaConexion, $usuario, $pass) {
+    $bd = new PDO($cadenaConexion, "root", "");
+    $bd->beginTransaction();
+    $ins = $consulta;
     $result = $bd->query($ins);
+    foreach ($result as $resultado) {
+        session_start();
+    }
+    return $result;
 }
 
-
-/*require 'vendor/autoload.php';
-try{
-    $cliente= new MongoDB\Client("mongodb://localhost:27017");
-    $bd = $cliente->libroservidor;
-    
-    //Se elimina el usuario llamado 'Paco'
-    $updateResult = $bd->usuarios->deleteOne(
-            ['nombre'=>'Pablo']
-    );
-    
-    echo "Documentos restantes después de borrar ".$bd->usuarios->count();
-    
-} catch (Exception $ex) {
-    print ($ex);
-}*/
+$consulta = consulta('select * from users where PASSWORD = sha1("'.$pass.'")&&USER="'.$usuario.'";', $cadenaConexion, $usuario, $pass);

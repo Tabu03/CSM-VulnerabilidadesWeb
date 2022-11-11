a25f505e44c1489bc180312bcd9e95f37576b1ac
carla <carlajilo@gmail.com>
2022-11-11 16:55:33 +0100

    added inicio

---
+++ Proyecto/incio.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Inicio</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="encabezado">Bienvenido al Goliath National Bank</h1>
+        <div class="botones">
+            <div class="contbtn">
+                <button class="btn">Ingresar dinero</button>
+                <button class="btn">Sacar dinero</button>
+                <button class="btn">Comprobar extracto de la cuenta</button>
+            </div>
+            <div class="contbtn">
+                <button class="btn">Realizar transferencia</button>
+                <button class="btn">Ver últimos movimientos</button>
+                <button class="btn">Sacar dinero sin tarjeta</button>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+

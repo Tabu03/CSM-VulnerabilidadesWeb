478fb5209c7529b3ee5cf9425526a8bba66ff78a
Tabu <tabulete@gmail.com>
2022-11-11 16:47:55 +0100

    Add styles

--- Proyecto/index.php
+++ Proyecto/index.php
@@ -7,17 +7,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Proyecto</title>
+        <link rel="stylesheet" href="./css/style.css">
     </head>
     <body>
+        <main class="container">
+        <section class="form">
         <form method ="POST" action ="
             <?php echo
             $_SERVER["PHP_SELF"];
             ?>">
-            Usuario
+            <p class="form__user">Usuario</p>
             <input id ="usuario" name="usuario" type="text">
+            <p class="form__paswd">Contraseña</p>
             <input id="clave" name="clave" type="password">
-            <input type="submit">
+            <input class="form__btn" type="submit">
         </form>
+        </section>
+</main>
     </body>
 </html>

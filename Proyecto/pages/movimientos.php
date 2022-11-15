e6045b6f394f666355268ae083e9ffeba8464b8c
carla <carlajilo@gmail.com>
2022-11-15 20:52:56 +0100

    added movimientos.php

---
+++ Proyecto/pages/movimientos.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movimientos</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="cont__log">
+        <span class="log__text">Logeado por:</span>
+        <button class="boton boton__log">Log out</button>
+    </div>
+    <table class="table">
+        <tr class="table__head">
+            <td>ID_Cliente</td>
+            <td>Nombre_Cliente</td>
+            <td>Saldo</td>
+        </tr>
+        <tr class="table__cont">
+            <td>sgd</td>
+            <td>dfg</td>
+            <td>sgd</td>
+        </tr>
+    </table>
+    <table class="table">
+        <tr class="table__head">
+            <td>ID_Transacción</td>
+            <td>ID_Cliente</td>
+            <td>Saldo modificado</td>
+            <td>Fecha</td>
+            <td>Descripción</td>
+        </tr>
+        <tr class="table__cont">
+            <td>sgd</td>
+            <td>dfg</td>
+            <td>dfg modificado</td>
+            <td>dfhf</td>
+            <td>dhgf</td>
+        </tr>
+    </table>
+    <div class="botones">
+        <button class="boton">Añadir cliente</button>
+        <button class="boton">Modificar cliente</button>
+        <button class="boton">Borrar cliente</button>
+    </div>
+</body>
+
+</html>
+

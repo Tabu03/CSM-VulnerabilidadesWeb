8a7ae06960af20bfdf448e1ed4689b4a226607da
Tabu <tabulete@gmail.com>
2022-11-16 16:59:52 +0100

    Styles added

--- Proyecto/pages/inicio.php
+++ Proyecto/pages/inicio.php
@@ -12,8 +12,11 @@ if(isset($_GET["hidden"])){
 <html>
 
 <head>
-    <meta charset="UTF-8">
-    <title>Inicio</title>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INICIO</title>
+    <link rel="stylesheet" href="../css/inicio.css">
 </head>
 
 <body>
--- Proyecto/pages/movimientos.php
+++ Proyecto/pages/movimientos.php
@@ -17,7 +17,7 @@ if(isset($_GET["hidden"])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movimientos</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/movimientos.css">
 </head>
 
 <body>
